6623509fcf08ceed610fcbd512f14f08d83f8bfe
Kase Branham <kase1111@gmail.com>
2026-03-18 23:12:56 -0700

    Add files via upload

---
+++ Add_RecycleBin_to_NavPane.bat
@@ -0,0 +1,18 @@
+@echo off
+:: Add Recycle Bin to File Explorer Navigation Pane
+:: Run as Administrator for best results
+
+echo Adding Recycle Bin to Navigation Pane...
+
+reg add "HKCU\Software\Classes\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}" /f >nul 2>&1
+reg add "HKCU\Software\Classes\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\ShellFolder" /v Attributes /t REG_DWORD /d 0x50000020 /f >nul 2>&1
+
+echo Done. Restarting Explorer...
+
+taskkill /f /im explorer.exe >nul 2>&1
+timeout /t 2 /nobreak >nul
+start explorer.exe
+
+echo.
+echo Recycle Bin should now appear in the Navigation Pane.
+pause
